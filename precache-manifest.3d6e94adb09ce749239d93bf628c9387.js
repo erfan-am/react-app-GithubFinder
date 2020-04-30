49740e5f098d11e7835cf540d66acbcb177cc45f
erfan-am <erfanam582@gmail.com>
2020-04-30 03:35:24 -0700

    Updates

---
+++ precache-manifest.3d6e94adb09ce749239d93bf628c9387.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34055606fbcf9f36501e3fcce787ea0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "358aeaea5cdd95960c44",
+    "url": "/static/css/main.21b84b3b.chunk.css"
+  },
+  {
+    "revision": "ab847df56b480466ff95",
+    "url": "/static/js/2.6f2001f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6f2001f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "358aeaea5cdd95960c44",
+    "url": "/static/js/main.1d7ef19f.chunk.js"
+  },
+  {
+    "revision": "1dc381130f73e8fd1ae3",
+    "url": "/static/js/runtime-main.143c2522.js"
+  }
+]);
